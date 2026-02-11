d1a63504fcb9e80d68e17f2e907ecf38d44b547b
renizens <grosheva.rens@gmail.com>
2026-02-11 18:44:48 +0300

    test

---
+++ lab1/src/Main.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        exercise1();
+    }
+    static void exercise1() {
+        Scanner scanner = new Scanner(System.in);
+
+        //ввод высоты
+        System.out.print("Введите высоту прямоугольника: ");
+        int h = scanner.nextInt();
+
+        //ввод ширины
+        System.out.print("Введите ширину прямоугольника: ");
+        int w = scanner.nextInt();
+
+        scanner.close();
+
+        //цикл по высоте
+        for (int i = 0; i < h; i++){
+            //цикл по ширине
+            for (int j = 0; j < w; j++) {
+                //находимся ли мы на верхней или нижней границе
+                if (i == 0 || i == h - 1) {
+                    //на верхней или нижней границе
+                    if (j == 0 || j == w - 1) {
+                        System.out.print('*');
+                    } else {
+                        System.out.print('-');
+                    }
+                }
+                //если не на границе
+                else {
+                    if (j == 0 || j == w - 1) {
+                        System.out.print('|');
+                    } else {
+                        System.out.print(' ');
+                    }
+                }
+            }
+            System.out.println(); //переход на новую строку
+        }
+    }
+}
